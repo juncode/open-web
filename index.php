bb14bdd4565e609d12d7b0e9f1ec6dfca9d53892
xiaoan <xiaoan@juanpi.com>
2015-07-24 10:35:07 +0800

    更改架构层方法

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ define('APP_DEBUG',True);
 // 定义应用目录
 define('APP_PATH','./Apps/');
 
-define('APP_ROOT', './');
+define('APP_ROOT', dirname(__FILE__) . '/');
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
 
